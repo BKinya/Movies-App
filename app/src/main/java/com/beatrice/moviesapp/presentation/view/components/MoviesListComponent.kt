f895796ffee5448a58d31533d9701bb9f0da2b12
Beatrice Kinya <gitongakinya1@gmail.com>
2022-11-14 07:12:42 +0300

    reset to default

--- app/src/main/java/com/beatrice/moviesapp/presentation/view/components/MoviesListComponent.kt
+++ app/src/main/java/com/beatrice/moviesapp/presentation/view/components/MoviesListComponent.kt
@@ -6,37 +6,23 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.beatrice.moviesapp.data.model.Movie
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun MoviesListComponent(
     movies: List<Movie>,
-    resultType: String = "Popular Movies",
-    navigateToMovieDetails: (movieId: Int) -> Unit = {}
 ) {
-    val gridState = rememberLazyGridState()
-    val showFab by remember {
-        derivedStateOf { gridState.firstVisibleItemIndex > 0 }
-    }
-    val coroutineScope = rememberCoroutineScope()
-
+    val showFab = false
     Scaffold(
         floatingActionButton = {
             AnimatedVisibility(
@@ -45,15 +31,12 @@ fun MoviesListComponent(
                 exit = scaleOut(),
             ) {
                 MoviesFab {
-                    coroutineScope.launch {
-                        gridState.animateScrollToItem(0)
-                    }
+
                 }
             }
         }
     ) { paddingValues ->
-        // Todo
-        val listState  = rememberLazyListState()
+
         /**
          * - Arrangement.spaceBy()
          * - Content Padding Vs Modifier.padding
@@ -64,8 +47,8 @@ fun MoviesListComponent(
          *    first item and last item in the list
          */
         LazyColumn(
-            state= listState,
             modifier = Modifier.padding(vertical = 48.dp, horizontal = 16.dp),
+
         ) {
             items(movies) { movie ->
                 MovieComponent(
@@ -73,33 +56,6 @@ fun MoviesListComponent(
                 )
             }
         }
-//        LazyVerticalGrid(
-//            columns = GridCells.Adaptive(256.dp),
-//            state = gridState,
-//            contentPadding = PaddingValues(top = 20.dp, bottom = 40.dp, start = 20.dp, end = 20.dp),
-//            verticalArrangement = Arrangement.spacedBy(16.dp),
-//            horizontalArrangement = Arrangement.spacedBy(8.dp)
-//        ) {
-//            item {
-//                Text(
-//                    text = resultType,
-//                    modifier = Modifier.fillMaxWidth(),
-//                    style = TextStyle(
-//                        fontWeight = FontWeight.SemiBold,
-//                        color = Color.DarkGray,
-//                        fontSize = 22.sp,
-//                        fontFamily = FontFamily.Serif
-//                    ),
-//                    textAlign = TextAlign.Center
-//                )
-//            }
-//            items(movies) { movie ->
-//                MovieComponent(
-//                    movie = movie,
-//                    navigateToMovieDetails = navigateToMovieDetails
-//                )
-//            }
-//        }
     }
 }
 
